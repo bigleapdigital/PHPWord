ada7191e85fbef2d4141077518038ec4e20728c4
Graham Vanderplank <grummbo@gmail.com>
2015-10-26 20:17:50 +0800

    Implement setImageValue on TemplateProcessor according to http://stackoverflow.com/questions/24018003/how-to-add-set-images-on-phpoffice-phpword-template

--- src/PhpWord/TemplateProcessor.php
+++ src/PhpWord/TemplateProcessor.php
@@ -336,6 +336,27 @@ class TemplateProcessor
     }
 
     /**
+     * Set a new image
+     *
+     * @param string $search
+     * @param string $replace
+     */
+    public function setImageValue($search, $replace)
+    {
+        // Sanity check
+        if (!file_exists($replace))
+        {
+            return;
+        }
+
+        // Delete current image
+        $this->zipClass->deleteName('word/media/' . $search);
+
+        // Add a new one
+        $this->zipClass->addFile($replace, 'word/media/' . $search);
+    }
+
+    /**
      * Saves the result document to the user defined file.
      *
      * @since 0.8.0
